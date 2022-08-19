e45238a720c00f7cd43400b87f2b90db15024eb2
Pradeep <pradeep2k@msn.com>
2022-08-20 00:13:35 +0530

    Updated code as per mojarra documentation..

---
+++ stalk-web.app/src/main/java/com/stalk/web/application/ActivateCDI.java
@@ -0,0 +1,11 @@
+package com.stalk.web.application;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import jakarta.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class ActivateCDI {
+
+}
